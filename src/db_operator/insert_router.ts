944ce243c66b3a0b94f3c898cb2b4ea71051824a
Shaltear-Lychee <49912764+Shaltear-Lychee@users.noreply.github.com>
2021-09-24 02:49:41 +0900

    Added validation system to insert_router

--- src/db_operator/insert_router.ts
+++ src/db_operator/insert_router.ts
@@ -1,54 +1,82 @@
 import { Request, Response, Router } from 'express'
 import { AnyError, InsertOneResult, MongoClient } from 'mongodb'
+import { body, validationResult } from 'express-validator'
 
 const insert_router = Router()
 const DB_NAME = 'testdb'
 const MONGODB_URI = 'mongodb://localhost:27017/testdb'
 
 insert_router.get('/', (req: Request, res: Response) => {
+  const form: {
+    article_title: string
+    article_content: string
+    article_author: string
+  } = {
+    article_title: '',
+    article_content: '',
+    article_author: ''
+  }
   res.render('./insert_ui', {
-    title: 'insert',
+    errs_arr: null,
+    form: form
   })
 })
 
-insert_router.post('/', (req: Request, res: Response) => {
-  MongoClient.connect(MONGODB_URI, (err?, db?) => {
-    console.log('hello')
-    if (err) {
-      throw err
-    } else {
-      if (db) {
-        const database = db.db(DB_NAME)
-        type article = {
-          id: number
-          title: string
-          content: string
-          author: string
-        }
-        database
-          .collection('articles')
-          .find()
-          .count()
-          .then((res1: number) => {
-            const article: article = {
-              id: res1,
-              title: req.body.article_title,
-              content: req.body.article_content,
-              author: req.body.article_author,
-            }
-            database.collection('articles').insertOne(article, (err?: AnyError, res2?: InsertOneResult<Document>) => {
-              if (err) {
-                throw err
-              } else {
-                console.log('insert successful. inserted: ' + res2)
-              }
-              db.close()
-              res.redirect('/')
+insert_router.post(
+  '/',
+  [
+    body('article_title').not().isEmpty().withMessage('You cannnot insert without title'),
+    body('article_content').not().isEmpty().withMessage('You cannot insert without the article content'),
+    body('article_content').not().isEmpty().withMessage('You cannot insert without your name'),
+  ],
+  (req: Request, res: Response) => {
+    MongoClient.connect(MONGODB_URI, (err?, db?) => {
+      if (err) {
+        throw err
+      } else {
+        if (db) {
+          const validation_errs = validationResult(req)
+          if (!validation_errs.isEmpty()) {
+            res.render('./insert_ui', {
+              errs_arr: validation_errs.array(),
+              form: req.body
             })
-          })
+          } else {
+            const database = db.db(DB_NAME)
+            type article = {
+              id: number
+              title: string
+              content: string
+              author: string
+            }
+            database
+              .collection('articles')
+              .find()
+              .count()
+              .then((res1: number) => {
+                const article: article = {
+                  id: res1,
+                  title: req.body.article_title,
+                  content: req.body.article_content,
+                  author: req.body.article_author,
+                }
+                database
+                  .collection('articles')
+                  .insertOne(article, (err?: AnyError, res2?: InsertOneResult<Document>) => {
+                    if (err) {
+                      throw err
+                    } else {
+                      console.log('insert successful. inserted: ' + res2)
+                    }
+                    db.close()
+                    res.redirect('/')
+                  })
+              })
+          }
+        }
       }
-    }
-  })
-})
+    })
+  }
+)
 
 export default insert_router
